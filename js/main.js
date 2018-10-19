2431e5c7d266b7f428738d497b59dfbd65d2b42f
catink123 <qwerty.qaz120@gmail.com>
2018-10-19 22:18:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 function link (path) {
-    $(".blurb").load(path + " .blurb");
+    $(".blurb").fadeOut(300, function () {
+        $(".blurb").load(path + " .blurb", function () {
+            $(".blurb").fadeIn(300);
+        });
+    });
 }
-
 /* function mute () {
     document.getElementById('player').muted = !document.getElementById('player').muted;
 } */
