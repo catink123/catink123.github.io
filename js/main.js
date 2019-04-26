16793fc11b057a6b597f703ee9fb2d23b3b143cd
Catink123 <qwerty.qaz120@gmail.com>
2019-04-26 21:36:57 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 function link (path) {
     $(".container").fadeOut(300, function () {
-        $(".loading").fadeIn(300);
+        $(".loading").fadeIn(150);
         $(".container").load(path + " .blurb", function () {
-            $(".loading").fadeOut(300, function () {
-                $(".container").fadeIn(300);
-            });
+            $(".loading").fadeOut(150);
+            $(".container").fadeIn(150);
             p = path.replace("index.html", "");
             history.pushState(p, p, p);
         });
     });
 }
-/* function mute () {
-    document.getElementById('player').muted = !document.getElementById('player').muted;
-} */
