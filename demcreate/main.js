f30d3ec02f4c9bdd1042fdbc04023c4045ae6e9c
Catink123 <qwerty.qaz120@gmail.com>
2020-07-27 01:47:18 +0300

    Update main.js

--- demcreate/main.js
+++ demcreate/main.js
@@ -51,33 +51,5 @@ function render() {
     canvas.width - 28 * scaling,
     canvas.height - 108 * scaling
   );
-  
-  function render() {
-  canvas.width = 400 * scaling;
-  canvas.height = ((img.height / img.width) * 400 + 104) * scaling;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.lineWidth = 2 * scaling;
-  ctx.strokeStyle = "white";
-  ctx.strokeRect(
-    10 * scaling,
-    10 * scaling,
-    canvas.width - 20 * scaling,
-    canvas.height - 100 * scaling
-  );
-  ctx.fillStyle = "white";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "top";
-  ctx.font = `${36 * scaling}px sans`;
-  ctx.fillText(firstRow.value, canvas.width / 2, canvas.height - 70 * scaling);
-  ctx.font = `${14 * scaling}px sans-serif`;
-  ctx.fillText(secondRow.value, canvas.width / 2, canvas.height - 30 * scaling);
-  ctx.drawImage(
-    img,
-    14 * scaling,
-    14 * scaling,
-    canvas.width - 28 * scaling,
-    canvas.height - 108 * scaling
-  );
-
   document.querySelector("#output").src = canvas.toDataURL();
 }
